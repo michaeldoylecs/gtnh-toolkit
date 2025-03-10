781abfc6b66c6768c24f02668e1296c46b2c0d0e
Michael Doyle <mcdoyle7227@gmail.com>
2025-03-10 15:48:04 -0400

    Readd variable debug info

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from pprint import pprint
+import pyomo.environ as pyomo # type: ignore
 from configreader import load_factory_config
 from grapher import build_solution_graph, draw
 from solver import solve
@@ -10,6 +12,10 @@ def main():
         print("Loaded config had errors")
         exit(1)
     model, results, machine_map = solve(factory_config.recipes, factory_config.targets[0])
+
+    # Debug model variables
+    variables = {v.name.strip("'"): v.value for v in model.component_objects(pyomo.Var, active=True) for v in v.values()}
+    pprint(variables)
 
     graph = build_solution_graph(model, machine_map)
     draw(graph)
